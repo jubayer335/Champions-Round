bc40a2171109af0232a132f23462a28f79e69045
jubayer335 <158508924+jubayer335@users.noreply.github.com>
2026-03-11 17:48:56 -0400

    offer-section done

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ if (seenOn.length > 0) {
         dots: false,
         infinite: true,
         speed: 1200, // Fast but smooth
-        slidesToShow: 3,
+        slidesToShow: 5,
         slidesToScroll: 1,
         arrows: false,
         autoplay: true,
@@ -22,7 +22,7 @@ if (seenOn.length > 0) {
                     dots: false,
                     infinite: true,
                     speed: 2000, // Fast but smooth
-                    slidesToShow: 2,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                     arrows: false,
                     autoplay: true,
@@ -31,12 +31,94 @@ if (seenOn.length > 0) {
                 }
             },
             {
-                breakpoint: 725,
+                breakpoint: 950,
+                settings: {
+                    dots: false,
+                    infinite: true,
+                    speed: 2000, // Fast but smooth
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 0, // No pause between slides
+                    cssEase: 'linear',
+                }
+            },
+             {
+                breakpoint: 600,
                 settings: {
                     dots: false,
                     infinite: true,
                     speed: 2000, // Fast but smooth
                     slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 0, // No pause between slides
+                    cssEase: 'linear',
+                }
+            }
+            
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+
+};
+
+
+let seenOn2 = $('.seen-on-slider-2');
+
+if (seenOn2.length > 0) {
+
+    $('.seen-on-slider-2').slick({
+        dots: false,
+        infinite: true,
+        speed: 1200, // Fast but smooth
+        slidesToShow: 9,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 0, // No pause between slides
+        cssEase: 'linear', // Smooth continuous movement
+        responsive: [
+            {
+                breakpoint: 840,
+                settings: {
+                    dots: false,
+                    infinite: true,
+                    speed: 2000, // Fast but smooth
+                    slidesToShow: 6,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 0, // No pause between slides
+                    cssEase: 'linear',
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    dots: false,
+                    infinite: true,
+                    speed: 2000, // Fast but smooth
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 0, // No pause between slides
+                    cssEase: 'linear',
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    dots: false,
+                    infinite: true,
+                    speed: 2000, // Fast but smooth
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                     arrows: false,
                     autoplay: true,
